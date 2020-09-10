44cd73b132a9648db2e0d5f8ceb1a1370bf9cb44
Steveen <steveen.echeverri@gmail.com>
2020-09-10 12:13:08 -0500

    Create test for sigle users

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,10 +2,22 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
-it('Respond with json containing a list of all users', done => {
-    request(app)
-        .get('/users')
-        .set('Accept', 'Application/json')
-        .expect('Content-type', /json/)
-        .expect(200, done);
+describe('GET /users', () => {
+    it('Respond with json containing a list of all users', done => {
+        request(app)
+            .get('/users')
+            .set('Accept', 'Application/json')
+            .expect('Content-type', /json/)
+            .expect(200, done);
+    });
+});
+
+describe('GET /users/:id', () => {
+    it('Respond with json containing a single users', (done) => {
+        request(app)
+            .get('/users/user01')
+            .set("Accept", "application/json")
+            .expect('Content-type', /json/)
+            .expect(200, done);
+    });
 });
